8982892a0296170b77516ce5a3f7adac6f0f3fb2
AnaghMiglani <150614061+AnaghMiglani@users.noreply.github.com>
2026-04-21 23:55:37 +0530

    EMI tools

---
+++ app/tools/math/emi.py
@@ -0,0 +1,23 @@
+from langchain.tools import tool
+
+@tool
+def emi(principal:int, rate:float, tenure:int):
+    """
+    Calculates EMI from principal amount, rate and tenure (in months).
+    Use this function whenever you need to calculate EMI.
+    Args:
+        principal (int): The total amount borrowed
+        rate (float): Monthly rate
+        tenure (int): Total number of months for repayment
+    """
+    return (principal*rate*(1+rate)**tenure)/((1+rate)**tenure)-1)
+
+@tool("covert_rate_from_annual_to_monthly")
+def convert_rate(rate:float):
+    """
+    Converts rate from annual to monthly.
+    Use this function whenever you need to convert rate from annual to monthly.
+    Args:
+        rate (float): Annual rate
+    """
+    return (rate/12*100)
