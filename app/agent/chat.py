8aac50643559b76882d6953c0766dcfff9048052
AnaghMiglani <150614061+AnaghMiglani@users.noreply.github.com>
2026-04-29 20:52:13 +0530

    Made chat agent into a function

--- app/agent/chat.py
+++ app/agent/chat.py
@@ -1,27 +1,23 @@
-from langchain_groq import ChatGroq
-from langchain.messages import SystemMessage,HumanMessage
-from dotenv import load_dotenv
-from app.tools.emi_options.main import get_emi_plans
-from app.tools.date.today import get_current_datetime_ist
-from app.tools.math.emi import emi,format_currency,convert_rate
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.agents import create_agent
-from langchain.agents.middleware import SummarizationMiddleware
-from langgraph.checkpoint.memory import InMemorySaver
-from langchain_core.runnables import RunnableConfig
-load_dotenv()
-import os
+def chat_ai(name:str,amount:str,emi_plan:str,last_contact:str,no_response_days:int,sentiment:str,user_prompt:str):
+    from langchain_groq import ChatGroq
+    from langchain.messages import SystemMessage,HumanMessage
+    from dotenv import load_dotenv
+    from app.tools.emi_options.main import get_emi_plans
+    from app.tools.date.today import get_current_datetime_ist
+    from app.tools.math.emi import emi,format_currency,convert_rate
+    from langchain_core.prompts import ChatPromptTemplate
+    from langchain.agents import create_agent
+    from langchain.agents.middleware import SummarizationMiddleware
+    from langgraph.checkpoint.memory import InMemorySaver
+    from langchain_core.runnables import RunnableConfig
+    import os
+    import json
 
-tools=[get_current_datetime_ist,emi,format_currency,convert_rate,get_emi_plans]
+    load_dotenv()
 
-name="anagh"
-amount="1500"
-emi_plan="None"
-last_contact="None"
-no_response_days=0
-sentiment="neutral"
+    tools=[get_current_datetime_ist,emi,format_currency,convert_rate,get_emi_plans]
 
-SYSTEM_PROMPT=f"""
+    SYSTEM_PROMPT=f"""
 You are a polite and professional collections assistant, communicating with users in India. 
 
 Your job is to generate a single message to a user based on their situation.
@@ -60,40 +56,64 @@ Encourage the user to take a step toward repayment while maintaining a positive 
 Output only the message text.
 """
 
-USER_PROMPT=f"""
+    llm=ChatGroq(
+        model="llama-3.3-70b-versatile",
+        temperature=0.2,
+        max_retries=2,
+        api_key=os.getenv("GROQ_API_KEY"),
+    )
+
+    summary_llm=ChatGroq(
+        model="llama-3.1-8b-instant",
+        temperature=0.1,
+        max_retries=4,
+        api_key=os.getenv("GROQ_API_KEY"),
+    )
+
+    checkpointer=InMemorySaver()
+
+    agent=create_agent(
+        model=llm,
+        tools=tools,
+        system_prompt=SYSTEM_PROMPT,
+        middleware=[
+            SummarizationMiddleware(
+                model=summary_llm,
+                trigger=("tokens",2000),
+                keep=("messages",10)
+            )
+        ],
+        checkpointer=checkpointer
+    )
+
+    config:RunnableConfig={"configurable":{"thread_id":"1"}}
+
+    response=agent.invoke({
+        "messages":[
+            HumanMessage(content=user_prompt)
+        ]
+    },config)
+
+    state=checkpointer.get(config)
+
+    with open("temp_char_structure.json","w") as f:
+        f.write(json.dumps(state,indent=2,default=str))
+
+    return response["messages"][-1].content
+
+
+if __name__=="__main__":
+    name="anagh"
+    amount="1500"
+    emi_plan="None"
+    last_contact="None"
+    no_response_days=0
+    sentiment="neutral"
+
+    user_prompt="""
 I can only spare 40 rs per month as others go into rent
 Pls suggest me EMI options such that they tell me - how much money i pay per month and for how long
 """
 
-llm=ChatGroq(
-    model="llama-3.3-70b-versatile",
-    temperature=0.2,
-    max_retries=2,
-    api_key=os.getenv("GROQ_API_KEY"),
-)
-
-checkpointer=InMemorySaver()
-
-agent=create_agent(
-    model=llm,
-    tools=tools,
-    system_prompt=SYSTEM_PROMPT,
-    middleware=[
-        SummarizationMiddleware(
-            model=llm,
-            trigger=("tokens",2000),
-            keep=("messages",10)
-        )
-    ],
-    checkpointer=checkpointer
-)
-
-config:RunnableConfig={"configurable":{"thread_id":"1"}}
-
-response=agent.invoke({
-    "messages":[
-        HumanMessage(content=USER_PROMPT)
-    ]
-},config)
-
-print(response["messages"][-1].content)
+    result=chat_ai(name,amount,emi_plan,last_contact,no_response_days,sentiment,user_prompt)
+    print(result)
